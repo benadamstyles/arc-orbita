ac2a88971005cde3cb1193bbb6242e8350e38ee2
Ben Styles <enquiries@leeds-ebooks.co.uk>
2018-03-08 13:17:14 +0000

    not using layout file

--- layouts/index.js
+++ layouts/index.js
@@ -1,29 +1,26 @@
-// @flow
+// // @flow
 
-import React, {type Node} from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
+// import React, {type Node} from 'react'
 
-import './index.css'
+// import './index.css'
 
-export const styles = {
-  container: {
-    padding: '1rem',
-  },
-}
+// export const styles = {
+//   container: {
+//     padding: '1rem',
+//   },
+// }
 
-const TemplateWrapper = ({children}: {children: () => Node}) => (
-  <div>
-    <Helmet
-      title="Arc Orbita"
-      meta={[
-        {name: 'description', content: 'Sample'},
-        {name: 'keywords', content: 'sample, something'},
-      ]}
-    />
-    <div style={styles.container}>{children()}</div>
-  </div>
-)
+// const TemplateWrapper = ({children}: {children: () => Node}) => (
+//   <div>
+//     <Helmet
+//       title="Arc Orbita"
+//       meta={[
+//         {name: 'description', content: 'Sample'},
+//         {name: 'keywords', content: 'sample, something'},
+//       ]}
+//     />
+//     <div style={styles.container}>{children()}</div>
+//   </div>
+// )
 
-export default TemplateWrapper
+// export default TemplateWrapper
